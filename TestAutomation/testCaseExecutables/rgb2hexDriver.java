25db09c9439796013898af25d0287d1146042698
Hollande Powell <hollandepowell@gmail.com>
2020-10-04 17:16:19 -0400

    Fixed compilation issue with rgb2hexDriver.java

--- TestAutomation/testCaseExecutables/rgb2hexDriver.java
+++ TestAutomation/testCaseExecutables/rgb2hexDriver.java
@@ -4,8 +4,8 @@ import java.awt.Color;
 public class rgb2hexDriver {
 	public static void main(String[] args) {
 		String input=args[0];
-		String newColor = ColorConverter.rgb2hex(input);
-		System.out.println(newColor);
+		Color c = Color.decode(input);
+		System.out.println(ColorConverter.rgb2Hex(c));
 			
 		
 	}
