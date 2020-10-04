7bf4c768ee08057ee7ea52675844d3dfa4c1ab3d
Hollande Powell <hollandepowell@gmail.com>
2020-10-04 16:49:44 -0400

    Add files via upload

--- TestAutomation/testCaseExecutables/getHueDriver.java
+++ TestAutomation/testCaseExecutables/getHueDriver.java
@@ -2,10 +2,10 @@ package testCaseExecutables;
 
 import java.awt.Color;
 
-public class getBrightnessDriver{
+public class getHueDriver{
 	public static void main(String[] args){
 		
 		Color c = Color.decode(args[0]);
-		System.out.printf("%.2f", ColorConverter.getBrightness(c));
+		System.out.printf("%.2f", ColorConverter.getHue(c));
 	}
 }
