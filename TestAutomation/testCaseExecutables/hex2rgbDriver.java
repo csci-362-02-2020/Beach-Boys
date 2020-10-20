162a7d80a4ae7721f567612c3c9d7dcc51e41f97
pamons12 <47860735+pamons12@users.noreply.github.com>
2020-10-20 17:02:15 -0400

    Add files via upload

--- TestAutomation/testCaseExecutables/hex2rgbDriver.java
+++ TestAutomation/testCaseExecutables/hex2rgbDriver.java
@@ -5,8 +5,14 @@ public class hex2rgbDriver {
 	public static void main(String[] args) {
 		String input=args[0];
 		Color newColor = ColorConverter.hex2Rgb(input);
-		System.out.println(newColor);
-			
+		if(newColor == null) {
+			System.out.println("null");
+		}else {
+			int redValue=newColor.getRed();
+			int greenValue=newColor.getGreen();
+			int blueValue=newColor.getBlue();
+			System.out.println("Red Value: "+redValue+"; Green Value: "+greenValue+"; Blue Value: "+blueValue);
+		}		
 		
 	}
 }
