493d3986ffe10ee4a02975527d4b52eefa52a250
pamons12 <47860735+pamons12@users.noreply.github.com>
2020-10-20 16:19:43 -0400

    Add files via upload

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -10,14 +10,48 @@ echo "All files in testCaseExecutable has been compiled"
 
 declare -a arr
 
-touch reports/results.txt
-> reports/results.txt
+#Creates the html file and clears it if there is anything in it
+touch reports/results.html
+> reports/results.html
 
+echo \<html\>\<head\>\<style\> >> reports/results.html
+
+#Set up table border in html
+echo table, th, td { >> reports/results.html
+
+echo border: 2px solid black\; >> reports/results.html
+echo border-collapse: collapse\; >> reports/results.html
+echo } >> reports/results.html
+
+#Add white space between data and row/colum lines
+echo th, td { >> reports/results.html
+echo padding: 5px\; >> reports/results.html
+echo } >> reports/results.html
+
+echo \</style\>\</head\>\<body\> >> reports/results.html
+
+
+#Set up first row of table
+echo \<table\> >> reports/results.html
+echo \<tr\> >> reports/results.html
+echo \<td\>"Test Number"\<\/td\> >> reports/results.html
+echo \<td\>"Requirement"\<\/td\> >> reports/results.html
+echo \<td\>"Component"\<\/td\> >> reports/results.html
+echo \<td\>"Method"\<\/td\> >> reports/results.html
+echo \<td\>"Input"\<\/td\> >> reports/results.html
+echo \<td\>"Expected Output"\<\/td\> >> reports/results.html
+echo \<td\>"Output"\<\/td\> >> reports/results.html
+echo \<td\>"Passed?"\<\/td\> >> reports/results.html
+echo \<\/tr\> >> reports/results.html
+
+#Read in each file in the testCase directory
 for file in testCases/*.txt
 do
  i=0;
+ echo \<tr\> >> reports/results.html
  while read -r line
  do
+  echo \<td\>$line\<\/td\> >> reports/results.html
   arr[$i]="$line"
   i=$((i+1))
  done < $file
@@ -28,32 +62,36 @@ do
   declare method=${arr[3]}
   declare input=${arr[4]}
   declare expected=${arr[5]}
+  declare output
   
  if [[ $method == "hex2rgbDriver" ]]
  then
-   java testCaseExecutables.hex2rgbDriver "$input" >> reports/results.txt
+   output=$(java testCaseExecutables.hex2rgbDriver "$input")
  fi
  
  if [[ $method == "getBrightnessDriver" ]]
  then
-   java testCaseExecutables.getBrightnessDriver "$input" >> reports/results.txt
+   output=$(java testCaseExecutables.getBrightnessDriver "$input")
  fi
 
  if [[ $method == "getSaturationDriver" ]]
  then
-   java testCaseExecutables.getSaturationDriver "$input" >> reports/results.txt
+   output=$(java testCaseExecutables.getSaturationDriver "$input")
  fi
 
  if [[ $method == "getHueDriver" ]]
  then
-   java testCaseExecutables.getHueDriver "$input" >> reports/results.txt
+   output=$(java testCaseExecutables.getHueDriver "$input")
  fi
 
  if [[ $method == "rgb2hexDriver" ]]
  then
-   java testCaseExecutables.rgb2hexDriver "$input" >> reports/results.txt
+   output=$(java testCaseExecutables.rgb2hexDriver "$input")
  fi
+ echo \<td\>$output\<\/td\> >> reports/results.html
+ echo \<\/tr\> >> reports/results.html
 done
 
-
+echo \<\/table\> >> reports/results.html
+echo \</body\>\</html\> >> reports/results.html
 rm -f testCaseExecutables/*.class
